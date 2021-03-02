9e0f6248175acb3df62e9f2477de949a64dd6031
Poresh2222 <st.yahor.poresh@gmail.com>
2021-03-02 19:50:55 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { MainAuthComponent } from './components/auth/main-auth/main-auth.component';
+import { BaseComponent } from './components/base/base.component';
+import { BasePageComponent } from './components/base-page/base-page.component';
+import { LoginButtonComponent } from './components/auth/login/login-button/login-button.component';
+import { LoginDialogComponent } from './components/auth/login/login-dialog/login-dialog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainAuthComponent,
+    BaseComponent,
+    BasePageComponent,
+    LoginButtonComponent,
+    LoginDialogComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
